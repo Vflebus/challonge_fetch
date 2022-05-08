d4d7bc9d56dee3c6d9f1df5851a794c50ee895b4
Vincent Flebus <vflebus@gmail.com>
2022-05-08 14:39:51 +0200

    portrait

--- index.js
+++ index.js
@@ -40,7 +40,8 @@ const fetch_challonge = async (tournoi) => {
                 player2: allParticipants.filter(participant => participant.participant.id == match.match.player2_id)[0].participant.display_name,
                 player2Img: allParticipants.filter(participant => participant.participant.id == match.match.player2_id)[0].participant.attached_participatable_portrait_url,
                 winner: allParticipants.filter(participant => participant.participant.id == match.match.winner_id)[0].participant.display_name,
-                score: match.match.scores_csv
+                score: match.match.scores_csv,
+                id: match.match.id
             };
            return newMatch
         })
